8f3b60e814799e6f2dd0161cc1114c2c55492262
Kyungyoon Kim <ruddbs5302@gmail.com>
2020-02-20 00:03:13 -0700

    Init commit

---
+++ app/src/main/java/edu/utah/cs4530/emergency/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package edu.utah.cs4530.emergency.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
