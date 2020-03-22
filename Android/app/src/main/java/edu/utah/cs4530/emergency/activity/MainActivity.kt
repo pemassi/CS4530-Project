72caccf82de2dc96c788a4c6815ec3c951ca5f1d
Kyungyoon Kim <ruddbs5302@gmail.com>
2020-03-22 01:13:22 -0600

    ADD: Add swipe button

--- Android/app/src/main/java/edu/utah/cs4530/emergency/activity/MainActivity.kt
+++ Android/app/src/main/java/edu/utah/cs4530/emergency/activity/MainActivity.kt
@@ -10,9 +10,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
 import edu.utah.cs4530.emergency.R
 
 class MainActivity : AppCompatActivity() {
@@ -25,11 +23,6 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
