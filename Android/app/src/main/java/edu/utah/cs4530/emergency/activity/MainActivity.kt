40e4f4fce746bfce0f90e85db7b5b7bc3fd75925
pic2725 <43197848+pic2725@users.noreply.github.com>
2020-03-24 16:14:13 -0600

    ADD: Icons

--- Android/app/src/main/java/edu/utah/cs4530/emergency/activity/MainActivity.kt
+++ Android/app/src/main/java/edu/utah/cs4530/emergency/activity/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home,
+                R.id.nav_contacts,
                 R.id.nav_gallery,
                 R.id.nav_slideshow,
                 R.id.nav_tools,
