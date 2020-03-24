57b3c26db3c8f78db25634784a7232be4a598ded
Kyungyoon Kim <ruddbs5302@gmail.com>
2020-03-24 00:54:15 -0600

    ADD: History View

--- Android/app/src/main/java/edu/utah/cs4530/emergency/activity/MainActivity.kt
+++ Android/app/src/main/java/edu/utah/cs4530/emergency/activity/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home,
+                R.id.nav_history,
                 R.id.nav_gallery,
                 R.id.nav_slideshow,
                 R.id.nav_tools,
