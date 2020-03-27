bad954f902345eac0551bb2fe704b12e4b520b5b
Kyungyoon Kim <ruddbs5302@gmail.com>
2020-03-26 22:48:35 -0600

    UPDATE: MainActivity design update

--- Android/app/src/main/java/edu/utah/cs4530/emergency/activity/SplashActivity.kt
+++ Android/app/src/main/java/edu/utah/cs4530/emergency/activity/SplashActivity.kt
@@ -1,17 +1,34 @@
 package edu.utah.cs4530.emergency.activity
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import edu.utah.cs4530.emergency.R
 
-class SplashActivity: AppCompatActivity()
+class SplashActivity: AppCompatActivity(), PermissionListener
 {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        TedPermission.with(applicationContext)
+            .setPermissionListener(this)
+            .setDeniedMessage("If you reject permission, we cannot track your current location.")
+            .setPermissions(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.READ_PHONE_STATE
+            )
+            .check()
+
+
+    }
+
+    override fun onPermissionGranted() {
         //Check user is logged in
         if(FirebaseAuth.getInstance().currentUser == null)
         {
@@ -21,7 +38,9 @@ class SplashActivity: AppCompatActivity()
         {
             startActivity(Intent(this, MainActivity::class.java))
         }
+    }
 
+    override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
         finish()
     }
 }
