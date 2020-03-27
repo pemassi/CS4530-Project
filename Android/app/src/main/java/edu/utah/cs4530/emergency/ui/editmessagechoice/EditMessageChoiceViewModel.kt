91f1525e9bd21080effdf688a99159b5795d363f
Jacqueline Rich <u1195336@utah.edu>
2020-03-26 22:20:30 -0600

    Edit Message: Have three fragments for this function now. Still need to hook up buttons.

---
+++ Android/app/src/main/java/edu/utah/cs4530/emergency/ui/editmessagechoice/EditMessageChoiceViewModel.kt
@@ -0,0 +1,7 @@
+package edu.utah.cs4530.emergency.ui.editmessagechoice
+
+import androidx.lifecycle.ViewModel
+
+class EditMessageChoiceViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ Android/app/src/main/java/edu/utah/cs4530/emergency/ui/editmessagetutorial/EditMessageTutorialViewModel.kt
@@ -0,0 +1,7 @@
+package edu.utah.cs4530.emergency.ui.editmessagetutorial
+
+import androidx.lifecycle.ViewModel
+
+class EditMessageTutorialViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
